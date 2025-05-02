8aba66856a89f8aadbbc5ceaea74d2d09ec1d37b
SpPag <spiridonpagiatis@gmail.com>
2025-05-02 16:40:56 +0300

    style(quiz): beautify the regex match result part of the quiz a little, including hover behaviour

--- src/components/MultipleChoiceQuizResultHighlighted.tsx
+++ src/components/MultipleChoiceQuizResultHighlighted.tsx
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
-
 const MultipleChoiceQuizResultHighlighted = (match: string) => {
-    return `<span class="bg-yellow-200 border border-stone-400 rounded px-1 hover:bg-yellow-300 transition-colors duration-200">${match}</span>`
+    return `<span class="border bg-yellow-100 border-stone-400 rounded px-1 hover:bg-stone-400 hover:border-stone-800 dark:md:hover:bg-violet-800 dark:md:hover:border-stone-500 transition-colors duration-200">${match}</span>`
 }
 
 export { MultipleChoiceQuizResultHighlighted };
