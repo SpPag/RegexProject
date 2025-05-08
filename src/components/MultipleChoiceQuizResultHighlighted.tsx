8100768401aef38d5b6350e812bd5eccbfada781
SpPag <spiridonpagiatis@gmail.com>
2025-05-08 14:29:37 +0300

    feat(ui): adjust the highlighted parts of the match's result for dark mode

--- src/components/MultipleChoiceQuizResultHighlighted.tsx
+++ src/components/MultipleChoiceQuizResultHighlighted.tsx
@@ -1,5 +1,5 @@
 const MultipleChoiceQuizResultHighlighted = (match: string) => {
-    return `<span class="border bg-yellow-100 border-stone-400 rounded px-1 hover:bg-stone-400 hover:border-stone-800 dark:bg-violet-700 dark:border-red-300 dark:hover:bg-violet-800 dark:hover:border-stone-500 transition-colors duration-200">${match}</span>`
+    return `<span class="border bg-yellow-200 border-stone-400 rounded px-1 hover:bg-yellow-300 hover:border-stone-800 dark:bg-violet-800 dark:border-zinc-500 dark:hover:bg-violet-700 dark:hover:border-stone-500 transition-colors duration-200">${match}</span>`
 }
 
 export { MultipleChoiceQuizResultHighlighted };
